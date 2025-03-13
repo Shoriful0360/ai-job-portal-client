7f2f557a3e58bf8bb7fb33a63af263aa3f7d1629
fahimuddinmoon <fahimmoon103@gmail.com>
2025-03-14 02:04:00 +0600

    delete redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
 import Router from './Router/Router'
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from 'react-redux'
-import store from './Utility/store'
+
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  
     <StrictMode>
       <BrowserRouter>
         <Router></Router>
       </BrowserRouter>
     </StrictMode>,
-  </Provider>
+  
 )
