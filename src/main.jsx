e542831eb4bd50887edb66bad48ccb3ac746919a
fahimuddinmoon <fahimmoon103@gmail.com>
2025-03-20 00:38:30 +0600

    route import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
 import Router from './Router/Router'
 import { BrowserRouter } from 'react-router'
 import AuthProvider from './Utility/AuthProvidor'
