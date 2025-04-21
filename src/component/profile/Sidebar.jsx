77ed0f19d82661b800162ab01c449fec00234643
Shoriful <shorifulbba0360@gmail.com>
2025-04-21 18:10:41 +0600

    added profile dynamic

--- src/component/profile/Sidebar.jsx
+++ src/component/profile/Sidebar.jsx
@@ -6,19 +6,22 @@ import { IoBookOutline } from "react-icons/io5";
 import { FiEdit } from "react-icons/fi";
 import { Link, NavLink } from "react-router";
 import { MdErrorOutline } from "react-icons/md";
+import useRole from "../../Utility/useRole";
 const Sidebar = () => {
+  const{role,isLoading}=useRole()
+  const{name,email,number,_id,photoUrl}=role || {}
     return (
         <div className="w-full  bg-[#1b132a] min-h-screen rounded-xl p-6 shadow-md">
         <div className="flex flex-col items-center">
           <div className="avatar">
             <div className="w-24 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
-              <img src="https://i.ibb.co/WxX0WkY/profile-img.png" alt="profile" />
+              <img src={photoUrl} alt="profile" />
             </div>
           </div>
-          <h2 className="mt-4 text-xl font-bold">MD Shoriful Islam</h2>
-          <p className="text-sm opacity-80">WEB10-1221</p>
-          <p className="text-sm">shorifulbba0360@gmail.com</p>
-          <p className="text-sm text-green-400">+8801307177507</p>
+          <h2 className="mt-4 text-xl font-bold">{name}</h2>
+          <p className="text-sm opacity-80">{`JVI-${_id.slice(-5)}`}</p>
+          <p className="text-sm">{email}</p>
+          <p className="text-sm text-green-400">+88{number}</p>
         </div>
         <div className="mt-6">
           <p className="text-sm flex justify-between">Complete your profile <span>100%</span></p>
