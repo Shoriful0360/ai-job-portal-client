e3562986c687e0116ffa75713eabfd34fb25d2f3
gr99murad <golamrabbanimurad@gmail.com>
2025-05-02 17:42:04 +0600

    Added private route and router

--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -1,27 +1,22 @@
+
 import { Navigate, useLocation } from "react-router";
 import LoadingSpinner from "../shared/LoadingSpinner";
 import { useSelector } from "react-redux";
 import useRole from "../Utility/useRole";
 
-const PrivateRoute = ({ children, allowedRoles = [] }) => {
-    const { user, loading } = useSelector((state) => state.auth);
-    const { role, isLoading } = useRole();
-    const location = useLocation();
 
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useSelector((state)=>state.auth)
+    const{role,isLoading}=useRole()
+    const location=useLocation()
     if (loading || isLoading) {
-        return <LoadingSpinner />;
+        return <LoadingSpinner/>
     }
-
-    if (!user) {
-        return <Navigate to="/login" state={{ from: location }} replace />;
+    if (user ||  role) {
+        return children
     }
-
-    if (allowedRoles.length === 0 || allowedRoles.includes(role)) {
-        return children;
-    }
-
-    // Optional: Navigate to an "unauthorized" page or dashboard
-    return <Navigate to="/unauthorized" replace />;
+    return <Navigate to='/login' state={{from:location}} replace="true"></Navigate>
 };
 
 export default PrivateRoute;
