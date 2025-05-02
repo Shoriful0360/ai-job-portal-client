803bbf23304698a2da10b74f7d0d44cec61973b5
gr99murad <golamrabbanimurad@gmail.com>
2025-05-02 18:57:04 +0600

    Update dashboard home page

--- src/page/DashBoardHome.jsx
+++ src/page/DashBoardHome.jsx
@@ -6,13 +6,14 @@ import StatisticsPage from "../Dashboard/StatisticsPage";
 const DashBoardHome = () => {
     const { user } = useContext(AuthContext)
     return (
-        <div className="py-16">
+        <div className="">
            
             <a href="https://git.io/typing-svg">
-                    <img className="my-8 " src={`https://readme-typing-svg.herokuapp.com/?lines=Hello+${user?.displayName}....;Welcome+to+you!&center=true&size=30`} alt="" />
+                    <img className="my-8" src={`https://readme-typing-svg.herokuapp.com/?lines=Hello+${user?.displayName}....;Welcome+to+you!&center=true&size=30`} alt="" />
                 </a>
                 <StatisticsPage></StatisticsPage>
         </div>
+
 
     );
 };
